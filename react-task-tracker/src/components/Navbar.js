c840f4ce20f1247bcc8f78b945f40f07f3117423
M.RAY <a.mray47@yahoo.fr>
2023-06-20 08:34:59 +0800

    styling the  app

--- react-task-tracker/src/components/Navbar.js
+++ react-task-tracker/src/components/Navbar.js
@@ -1,16 +1,22 @@
+import logo from "../logo.svg";
 const Navbar = () => {
   return (
-		<nav className="{style}">
-			<img />
-			<div>Learn</div>
-			<div>Learn React</div>
-			<div>Learn</div>
-			<div>Learn</div>
-		</nav>
-	)
+    <nav className="{style}">
+      <img className="" src={logo} />
+
+      <div className="nav-right">
+        <ul>
+          <li>Learn</li>
+          <li>Learn React</li>
+          <li>Learn</li>
+          <li>Learn</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
-const style =  {
-	gap: '20px'
-}
+const style = {
+  gap: "20px",
+};
 export default Navbar;
 
