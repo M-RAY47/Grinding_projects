600257f1b005fbc042cfdf6794ce8a056b22df17
M.RAY <a.mray47@yahoo.fr>
2023-06-25 23:11:28 +0800

    improve the code

--- react-task-tracker/src/components/Main.js
+++ react-task-tracker/src/components/Main.js
@@ -1,20 +1,20 @@
 const Main = () => {
-	const mainQuotes = []
+  const title = "Fun Facts about programming"
+  const mainQuotes = [
+    "Pain Can Keep the mind awake.",
+    "Very little is needed to make happy life",
+    "The Impediment to action advances action. What stands in the way becomes the way",
+    "Do something everyday that you don't to do, this is the golden rule for aquiring the habit of doing your duty",
+  ];
   return (
     <main>
       <div>
-        <div className="title">Fun Facts about programming</div>
+        <div className="title">{title}</div>
         <ul className="main-content">
-          <li>Pain Can Keep the mind awake.</li>
-          <li>Very little is needed to make happy life</li>
-          <li>
-            The Impediment to action advances action. What stands in the way
-            becomes the way
-          </li>
-          <li>
-            Do something everyday that you don't to do, this is the golden rule
-            for aquiring the habit of doing your duty
-          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
     </main>
