b4bf4693c017dc6bb9e18e22c6085496cbd63749
M.RAY <a.mray47@yahoo.fr>
2023-06-27 23:31:13 +0800

    improve the code

--- react-task-tracker/src/components/Main.js
+++ react-task-tracker/src/components/Main.js
@@ -5,8 +5,8 @@ const Main = () => {
     "Very little is needed to make happy life",
     "The Impediment to action advances action. What stands in the way becomes the way",
     "Do something everyday that you don't want to do, this is the golden rule for aquiring the habit of doing your duty",
-		"Don't judge your gretness by your shadow at the sunset"
-    ,'Never discouraged any one who continually makes progress no matter how slow it is, even if it is yourself'
+    "Don't judge your gretness by your shadow at the sunset",
+    "Never discouraged any one who continually makes progress no matter how slow it is, even if it is yourself",
   ];
   const main = mainQuotes.map((list) => <li key={list}>{list}</li>);
   return (
