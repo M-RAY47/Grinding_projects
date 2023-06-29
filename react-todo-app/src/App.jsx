273098005c5371cdc9cd468bfe15af31ab82c2da
M.RAY <a.mray47@yahoo.fr>
2023-06-29 23:05:28 +0800

    react-todo-app created the todo list first draft

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  // const [count, setCount] = useState(0)
+  const inputLabel = 'New Item'
+  const listHeader = "Todo List"
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <form className="new-item-form">
+      <div className="">
+        <label htmlFor='item' className="">{ inputLabel }</label>
+        <input id="item" className="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </form> 
+      <h1>{}</h1>
+      <div className="todo-list">
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
