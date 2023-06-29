0e782eb6c23bba874eebae4d235639b68712f99c
M.RAY <a.mray47@yahoo.fr>
2023-06-29 23:12:56 +0800

    added the input and bound it to the propers value

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  // const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("")
+
+  // setNewItem()
   const inputLabel = 'New Item'
   const listHeader = "Todo List"
   return (
     <>
      <form className="new-item-form">
       <div className="">
-        <label htmlFor='item' className="">{ inputLabel }</label>
-        <input id="item" className="" />
+        <label htmlFor='item' className="">{inputLabel}</label>
+        <input id="item" value={newItem} onChange=(e => setNewItem(e.target.value)) className="" />
       </div>
       </form> 
-      <h1>{}</h1>
+      <h1>{listHeader}</h1>
       <div className="todo-list">
         
       </div>
