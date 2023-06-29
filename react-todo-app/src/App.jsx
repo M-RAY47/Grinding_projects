e8092a56a53ed485d82ead0b663dc98394720b0c
M.RAY <a.mray47@yahoo.fr>
2023-06-29 23:19:25 +0800

    added todo list on the dsipled list

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -3,21 +3,23 @@ import './App.css'
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
-
+  const [todoList, setTodoList] = useState(['Sleep', 'Restart'])
   // setNewItem()
   const inputLabel = 'New Item'
   const listHeader = "Todo List"
   return (
     <>
-     <form className="new-item-form">
-      <div className="">
-        <label htmlFor='item' className="">{inputLabel}</label>
-        <input id="item" value={newItem} onChange=(e => setNewItem(e.target.value)) className="" />
-      </div>
+      <form className="new-item-form">
+        <div className="">
+          <label htmlFor='item' className="">{inputLabel}</label>
+          <input id="item" value={newItem} onChange={e => setNewItem(e.target.value)} className="" />
+        </div>
       </form> 
       <h1>{listHeader}</h1>
       <div className="todo-list">
-        
+        <ul>
+          {todoList.map((todo) => <li>{todo}</li>)}
+        </ul>
       </div>
     </>
   )
