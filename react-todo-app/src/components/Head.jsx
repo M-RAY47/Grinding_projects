d69798676651eb4365ffc141bf61093b2640f863
M.RAY <a.mray47@yahoo.fr>
2023-07-10 23:46:13 +0800

    added head component image

--- react-todo-app/src/components/Head.jsx
+++ react-todo-app/src/components/Head.jsx
@@ -1,8 +1,9 @@
+import logo from '../assets/react.svg'
 export default function Head () {
 	return (
 	<>
 	<div>
-		<img />
+		<img src={logo} />
 		<span > Good Evening M.RAY</span>
 	</div>
 	</>
