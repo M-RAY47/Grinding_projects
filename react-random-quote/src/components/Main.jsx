84e5db25a43bce7f3cd26b39d5bd6f696f749b15
M.RAY <a.mray47@yahoo.fr>
2024-03-24 13:39:38 +0800

    change to fecth quotes

--- react-random-quote/src/components/Main.jsx
+++ react-random-quote/src/components/Main.jsx
@@ -1,24 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-const Main = () => {
-	// colores
-	const colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
-]
+const Main = (props) => {
 
 	const [quotesData, setQuoteData] = useState({})
-	let currQuote = ''
+	const [currQuote, setCurrQuote] = useState()
+	
 	// get random quote from quoutes
 	const getRandomQuote = () => {
 		return quotesData.quotes[
@@ -29,20 +15,30 @@ const Main = () => {
 		const url ='https://my-json-server.typicode.com/M-RAY47/Quote-data/quotes'
 		const url1 ='https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 		
-		axios.get(url1)
-      .then(res => {
-        console.log('res.data', res.data, typeof res.data)
-				// const quotes = res.data
-				// setQuoteData(quotes)
-				currQuote = getRandomQuote()
-      })
-	})
+		const getData = async() => {
+			const res = await (await fetch(url1)).json()
+			console.log('The res>>>><>>>>>', res)
+		}
+		getData()
+		// axios.get(url1)
+    //   .then(res => {
+    //     console.log('res.data', res.data, typeof res.data, props)
+
+		// 		const quotes = res.data
+		// 		setQuoteData(quotes)
+		// 		// const currQuote = getRandomQuote()
+		// 		// setCurrQuote(cc)
+		// 		console.log('heree>>',  currQuote, quotesData)
+    //   })
+	}, [])
+
+	// const currQuote = getRandomQuote()
 const tweetLink = ''
 	return (
 		<main id="quote-box">
 			<div id="text">{currQuote}</div>
 			<div id="author"></div>
-			<button id="new-quote">New quote</button>
+			<button style={{color: props.uniqueColor}} id="new-quote">New quote</button>
 			<a href={tweetLink} id="tweet-quote">Tweet quote</a>
 		</main>
 	)
