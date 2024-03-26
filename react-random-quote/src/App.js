1f1f41155fac47f81f9b6909eaf3e86570a2f723
M.RAY <a.mray47@yahoo.fr>
2024-03-26 15:19:54 +0800

    modified the app.js

--- react-random-quote/src/App.js
+++ react-random-quote/src/App.js
@@ -1,15 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/Main';
+import "./App.css";
+import Main from "./components/Main.jsx";
 
 function App() {
+  // colores
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ];
+
+  const getRandomColor = () =>
+    colors[Math.floor(Math.random() * colors.length)];
+  // console.log();
+  const uniqueColor = getRandomColor();
+  const propsMain = {
+    uniqueColor
+  };
+  const appHeaderStyle = {
+    backgroundColor: uniqueColor,
+    // color: uniqueColor,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <Main />
+      <header className="App-header" style={appHeaderStyle}>
+        <Main props={propsMain} />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
