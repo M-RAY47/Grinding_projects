b2bb1f0cef952b1ac0066ded0280c4aadeae4fe8
M.RAY <a.mray47@yahoo.fr>
2024-03-26 15:17:58 +0800

    added main quote-box

--- react-random-quote/src/components/Main.jsx
+++ react-random-quote/src/components/Main.jsx
@@ -1,48 +1,85 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import TweetLogo from './TweetLogo.jsx'
 const Main = (props) => {
 
-	const [quotesData, setQuoteData] = useState({})
-	const [currQuote, setCurrQuote] = useState()
-	
-	// get random quote from quoutes
-	const getRandomQuote = () => {
-		return quotesData.quotes[
-    	Math.floor(Math.random() * quotesData.quotes.length)
-  	]
-	}
-	
+  // get random quote from quoutes
+  const getRandomQuote = (quotes) => {
+    return quotes[
+      Math.floor(Math.random() * quotes.length)
+    ];
+  };
+
+  const styleMain = {};
+
+  const btnClass = {
+    background: props.props.uniqueColor,
+    color: "#fff",
+  };
+
   useEffect(() => {
-		const url ='https://my-json-server.typicode.com/M-RAY47/Quote-data/quotes'
-		const url1 ='https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-		
-		// const getData = async() => {
-		// 	const res = await (await fetch(url1)).json()
-		// 	console.log('The res>>>><>>>>>', res)
-		// }
-		// getData()
-		axios.get(url1)
-      .then(res => {
-        console.log('res.data', res.data, typeof res.data, props)
+    // const url = "https://my-json-server.typicode.com/M-RAY47/Quote-data/quotes";
+    const url1 =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
-				const quotes = res.data
-				setQuoteData(quotes)
-		// 		// const currQuote = getRandomQuote()
-		// 		// setCurrQuote(cc)
-		// 		console.log('heree>>',  currQuote, quotesData)
-      })
-	}, [])
+    // const getData = async() => {
+    // 	const res = await (await fetch(url1)).json()
+    // 	console.log('The res>>>><>>>>>', res)
+    // }
+    // getData()
+    axios.get(url1).then((res) => {
+      // console.log("res.data", res.data, typeof res.data, props, props.props);
 
-	// const currQuote = getRandomQuote()
-const tweetLink = ''
-const newQuoteBtn = 'New Quote'
-	return (
-		<main id="quote-box">
-			<div id="text">{currQuote}</div>
-			<div id="author"></div>
-			<button style={{color: props.uniqueColor}} id="new-quote">{newQuoteBtn}</button>
-			<a href={tweetLink} id="tweet-quote">Tweet quote</a>
-		</main>
-	)
-}
-export default Main
+      const quotes = res.data.quotes;
+			console.log("quotes", quotes)
+      setQuotesData(quotes);
+			setCurrQuote(getRandomQuote(quotesData))
+			console.log(currQuote, quotesData)
+    });
+  }, []);
+
+  const [quotesData, setQuotesData] = useState([]);
+  const [currQuote, setCurrQuote] = useState(getRandomQuote(quotesData));
+
+	// console.log('curr', currQuote, quotesData, btnClass)
+	const changeQuote = () => {
+		setCurrQuote(getRandomQuote(quotesData))
+	}
+
+  const tweetQuote = '';
+  const newQuoteBtn = "New Quote";
+  return (
+    <main id="quote-box">
+      {/* <div id="text">{currQuote.quote}</div> */}
+      <div className="quote__info">
+        <div className="right-info">
+          <button
+            onClick={changeQuote}
+            className="btn"
+            style={btnClass}
+            id="new-quote"
+          >
+            {newQuoteBtn}
+          </button>
+          <a
+            id="tweet-quote"
+            className="btn"
+            style={btnClass}
+            href={tweetQuote}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TweetLogo props={props.props.uniqueColor} />
+            
+          </a>
+        </div>
+
+        {/* <div id="author">{currQuote.author}</div> */}
+      </div>
+    </main>
+  );
+};
+export default Main;
+
+
+
