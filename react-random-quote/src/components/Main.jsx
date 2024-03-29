ed88b4152306eff424dac9683967af4003b2a409
M.RAY <a.mray47@yahoo.fr>
2024-03-29 17:35:27 +0800

    fix typescript error

--- react-random-quote/src/components/Main.jsx
+++ react-random-quote/src/components/Main.jsx
@@ -5,7 +5,7 @@ import { getData } from "./getData.jsx";
 const url1 =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
-const Main = ({ uniqueColor, changeColor }: props) => {
+const Main = ({ uniqueColor, changeColor }) => {
   // get random quote from quoutes
   const getRandomQuote = (quotes) => {
     return quotes[Math.floor(Math.random() * quotes.length)];
